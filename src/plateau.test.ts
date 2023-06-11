9f768702b8f07f0cdcc8c23c732907c7cb81a144
Brian <brian@vvgonline.com>
2023-06-11 18:21:27 +0100

    feat: create array based on x,y input being >0,>0

--- src/plateau.test.ts
+++ src/plateau.test.ts
@@ -1,7 +1,14 @@
 import { setPlateauArea } from "./plateau";
 
-describe("Create a 2D array of consecutive integers based on an x, y parameters", () => {
+describe("Create a 2D array of param string based on an x, y parameters", () => {
   test("create empty array if parameters are  0,0", () => {
     expect(setPlateauArea(0, 0)).toEqual([]);
+  });
+  test("create 2D array populated with param fill if parameters are  >0,>0", () => {
+    expect(setPlateauArea(3, 3)).toEqual([
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ]);
   });
 });
--- src/plateau.ts
+++ src/plateau.ts
@@ -1,3 +1,13 @@
-export function setPlateauArea(x: number, y: number) {
-  return [];
+const x = 3;
+const y = 3;
+
+export function setPlateauArea(x: number, y: number, fill: string = " ") {
+  const plateauArea = Array(x)
+    .fill(fill)
+    .map(() => Array(y).fill(fill));
+  console.log(plateauArea);
+
+  return plateauArea;
 }
+
+setPlateauArea(x, y);
